ada1c11b13d1f7d40eba2f9a89a327328e165690
Martti Louhivuori <martti.louhivuori@csc.fi>
2020-11-24 09:37:02 +0200

    Split into two steps to sum over multiple blocks

--- cuda/dot.cu
+++ cuda/dot.cu
@@ -1,15 +1,43 @@
 #include <stdio.h>
 #include <cuda_runtime.h>
 
-__global__ void dot_(int n, double *x, double *y, double *z)
+__global__ void sum_(int n, double *x, double *y)
 {
-    int tid = threadIdx.x + blockIdx.x * blockDim.x;
+    int tid = threadIdx.x;
+    int gid = threadIdx.x + blockIdx.x * blockDim.x;
     int stride = gridDim.x * blockDim.x;
     extern __shared__ double buffer[];
     int i;
 
     double sum = 0.0f;
-    for (i = tid; i < n; i += stride) {
+    for (i = gid; i < n; i += stride) {
+        sum += x[i];
+    }
+    buffer[tid] = sum;
+    __syncthreads();
+
+    i = blockDim.x / 2;
+    while (i > 0) {
+        if (tid < i)
+            buffer[tid] += buffer[tid + i];
+        i = i / 2;
+        __syncthreads();
+    }
+
+    if (tid == 0)
+        y[blockIdx.x] = buffer[0];
+}
+
+__global__ void dot_(int n, double *x, double *y, double *z)
+{
+    int tid = threadIdx.x;
+    int gid = threadIdx.x + blockIdx.x * blockDim.x;
+    int stride = gridDim.x * blockDim.x;
+    extern __shared__ double buffer[];
+    int i;
+
+    double sum = 0.0f;
+    for (i = gid; i < n; i += stride) {
         sum += x[i] * y[i];
     }
     buffer[tid] = sum;
@@ -24,7 +52,7 @@ __global__ void dot_(int n, double *x, double *y, double *z)
     }
 
     if (tid == 0)
-        *z = buffer[0];
+        z[blockIdx.x] = buffer[0];
 }
 
 int main(void)
@@ -33,7 +61,7 @@ int main(void)
     const int n = 10000;
     double x[n], y[n];
     double z, z_ref;
-    double *x_, *y_, *z_;
+    double *x_, *y_, *z_, *buffer_;
 
     // initialise data and calculate reference values on CPU
     z_ref = 0.0f;
@@ -47,11 +75,13 @@ int main(void)
     cudaMalloc((void **) &x_, sizeof(double) * n);
     cudaMalloc((void **) &y_, sizeof(double) * n);
     cudaMalloc((void **) &z_, sizeof(double));
+    cudaMalloc((void **) &buffer_, sizeof(double) * 32);
     cudaMemcpy(x_, x, sizeof(double) * n, cudaMemcpyHostToDevice);
     cudaMemcpy(y_, y, sizeof(double) * n, cudaMemcpyHostToDevice);
 
     // calculate axpy on GPU
-    dot_<<<1,32*256>>>(n, x_, y_, z_);
+    dot_<<<32,256>>>(n, x_, y_, buffer_);
+    sum_<<<1,32>>>(32, buffer_, z_);
 
     // copy result back to host and print with reference
     cudaMemcpy(&z, z_, sizeof(double), cudaMemcpyDeviceToHost);
