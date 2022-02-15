8408d85ce388d5ad5665969891b840fb552c8288
Martti Louhivuori <martti.louhivuori@csc.fi>
2022-02-15 16:45:03 +0200

    Run axpy from python using a CUDA back-end

---
+++ cuda-python/axpy.cu
@@ -0,0 +1,88 @@
+#define PY_SSIZE_T_CLEAN
+#include <Python.h>
+#include <stdio.h>
+#include <cuda_runtime.h>
+
+__global__ void daxpy_(int n, double a, double *x, double *y)
+{
+    int tid = threadIdx.x + blockIdx.x * blockDim.x;
+    int stride = gridDim.x * blockDim.x;
+
+    for (; tid < n; tid += stride) {
+        y[tid] += a * x[tid];
+    }
+}
+
+__global__ void saxpy_(int n, float a, float *x, float *y)
+{
+    int tid = threadIdx.x + blockIdx.x * blockDim.x;
+    int stride = gridDim.x * blockDim.x;
+
+    for (; tid < n; tid += stride) {
+        y[tid] += a * x[tid];
+    }
+}
+
+void daxpy(int n, double a, double *x_, double *y_)
+{
+    daxpy_<<<32,256>>>(n, a, x_, y_);
+}
+
+void saxpy(int n, float a, float *x_, float *y_)
+{
+    saxpy_<<<32,256>>>(n, a, x_, y_);
+}
+
+extern "C"
+PyObject* daxpy_wrapper(PyObject *self, PyObject *args)
+{
+    int n;
+    double a;
+    double *x;
+    double *y;
+
+    if (!PyArg_ParseTuple(args, "idnn", &n, &a, &x, &y))
+        return NULL;
+
+    daxpy(n, a, x, y);
+
+    Py_RETURN_NONE;
+}
+
+extern "C"
+PyObject* saxpy_wrapper(PyObject *self, PyObject *args)
+{
+    int n;
+    float a;
+    float *x;
+    float *y;
+
+    if (!PyArg_ParseTuple(args, "ifnn", &n, &a, &x, &y))
+        return NULL;
+
+    saxpy(n, a, x, y);
+
+    Py_RETURN_NONE;
+}
+
+extern "C"
+static PyMethodDef methods[] = {
+    {"daxpy",  daxpy_wrapper, METH_VARARGS, "Daxpy"},
+    {"saxpy",  saxpy_wrapper, METH_VARARGS, "Saxpy"},
+    {NULL, NULL, 0, NULL}
+    };
+extern "C"
+static struct PyModuleDef module = {
+    PyModuleDef_HEAD_INIT,
+    "axpy",
+    NULL,
+    -1,
+    methods
+};
+
+extern "C"
+PyMODINIT_FUNC
+PyInit__axpy(void)
+{
+    return PyModule_Create(&module);
+}
---
+++ cuda-python/axpy.py
@@ -0,0 +1,38 @@
+from _axpy import daxpy
+from _mem import Dallocate, deallocate, Dmemcpy_h2d, Dmemcpy_d2h
+from math import sin, cos
+from array import array
+
+n = 10000;
+a = 3.4;
+
+# initialise data and calculate reference values on CPU
+x = array('d', [sin(i) * 2.3 for i in range(n)])
+y = array('d', [cos(i) * 1.1 for i in range(n)])
+y_ref = [a * i + j for (i,j) in zip(x,y)]
+
+# memory addresses of buffers
+xp = x.buffer_info()[0]
+yp = y.buffer_info()[0]
+
+# allocate + copy initial values
+x_ = Dallocate(n)
+y_ = Dallocate(n)
+Dmemcpy_h2d(x_, xp, n);
+Dmemcpy_h2d(y_, yp, n);
+
+# calculate axpy on GPU
+daxpy(n, a, x_, y_);
+
+# copy result back to host and print with reference
+print('  initial: {0} {1} {2} {3} {4} {5}'.format(
+    y[0], y[1], y[2], y[3], y[-2], y[-1]))
+Dmemcpy_d2h(yp, y_, n);
+print('reference: {0} {1} {2} {3} {4} {5}'.format(
+    y[0], y[1], y[2], y[3], y[-2], y[-1]))
+print('   result: {0} {1} {2} {3} {4} {5}'.format(
+    y[0], y[1], y[2], y[3], y[-2], y[-1]))
+
+# deallocate
+deallocate(x_)
+deallocate(y_)
