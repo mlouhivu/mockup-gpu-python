aab7cc5188706230f323219461a02d7f62dfaef1
Martti Louhivuori <martti.louhivuori@csc.fi>
2022-02-16 10:04:51 +0200

    Remove C-like semicolons

--- cuda-python/axpy.py
+++ cuda-python/axpy.py
@@ -18,16 +18,16 @@ yp = y.buffer_info()[0]
 # allocate + copy initial values
 x_ = Dallocate(n)
 y_ = Dallocate(n)
-Dmemcpy_h2d(x_, xp, n);
-Dmemcpy_h2d(y_, yp, n);
+Dmemcpy_h2d(x_, xp, n)
+Dmemcpy_h2d(y_, yp, n)
 
 # calculate axpy on GPU
-daxpy(n, a, x_, y_);
+daxpy(n, a, x_, y_)
 
 # copy result back to host and print with reference
 print('  initial: {0} {1} {2} {3} {4} {5}'.format(
     y[0], y[1], y[2], y[3], y[-2], y[-1]))
-Dmemcpy_d2h(yp, y_, n);
+Dmemcpy_d2h(yp, y_, n)
 print('reference: {0} {1} {2} {3} {4} {5}'.format(
     y_ref[0], y_ref[1], y_ref[2], y_ref[3], y_ref[-2], y_ref[-1]))
 print('   result: {0} {1} {2} {3} {4} {5}'.format(
