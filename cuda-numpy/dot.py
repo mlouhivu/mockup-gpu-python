91d98bf6c6b119100e2438ee66d458b5ce4c8618
Martti Louhivuori <martti.louhivuori@csc.fi>
2022-02-17 14:12:55 +0200

    CUDA dot product with numpy arrays

---
+++ cuda-numpy/dot.py
@@ -0,0 +1,28 @@
+from _dot import dot
+from _mem import Dallocate, deallocate, Dmemcpy_h2d, Dpeek, Dmemcpy_d2h
+from math import sin, cos
+from numpy import array
+
+blocks = 32;
+threads = 256;
+n = 10000;
+
+# initialise data and calculate reference values on CPU
+x = array([sin(i) * 2.3 for i in range(n)], float)
+y = array([cos(i) * 1.1 for i in range(n)], float)
+z_ref = sum(x * y)
+
+# allocate + copy initial values
+x_ = Dallocate(n)
+y_ = Dallocate(n)
+z_ = Dallocate(1)
+Dmemcpy_h2d(x_, x, n)
+Dmemcpy_h2d(y_, y, n)
+
+# calculate dot product on GPU (and reduce partial sums)
+dot(blocks, threads, n, x_, y_, z_)
+
+# copy result back to host and print with reference
+z = Dpeek(z_)
+print(' reference: {0}'.format(z_ref))
+print('    result: {0}'.format(z))
