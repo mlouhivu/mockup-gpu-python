84062929d1d60f280c7893b4bc1c83c8c28d7c10
Martti Louhivuori <martti.louhivuori@csc.fi>
2022-12-20 17:29:19 +0200

    Use timeit to time it

--- hipblas-cupy/async.py
+++ hipblas-cupy/async.py
@@ -1,10 +1,12 @@
 from math import sin, cos
+import timeit
 
 from numpy import array
 import cupy
 
 from _async import daxpy_async
 from _async import create_handle, destroy_handle
+from _axpy import daxpy
 
 n = 10000
 a = 3.4
@@ -24,15 +26,16 @@ y_ = cupy.asarray(y)
 a_ = cupy.asarray(array([a], float))
 
 # calculate reference values on CPU
-for i in range(repeat):
-    axpy(a, x, y)
+t_cpu = timeit.repeat('axpy(a, x, y)',
+                      number=repeat, repeat=1, globals=globals())
 
 # create BLAS handle
 handle = create_handle()
 
 # calculate axpy on GPU and keep data on GPUs
-for i in range(repeat):
-    daxpy_async(handle, n, a_.data.ptr, x_.data.ptr, y_.data.ptr)
+t_async = timeit.repeat(
+        'daxpy_async(handle, n, a_.data.ptr, x_.data.ptr, y_.data.ptr)',
+        number=repeat, repeat=1, globals=globals())
 
 # copy result back to host and print with reference
 print('  initial: {0} {1} {2} {3} {4} {5}'.format(
@@ -42,6 +45,23 @@ print('reference: {0} {1} {2} {3} {4} {5}'.format(
 y_.get(out=y)
 print('   result: {0} {1} {2} {3} {4} {5}'.format(
     y[0], y[1], y[2], y[3], y[-2], y[-1]))
+
+# calculate axpy on GPU without keeping data on GPUs
+y[:] = y_orig[:]
+y_ = cupy.asarray(y)
+t_sync = timeit.repeat(
+        'daxpy(n, a, x_.data.ptr, y_.data.ptr)',
+        number=repeat, repeat=1, globals=globals())
+
+# copy result back to host and print with reference
+y_.get(out=y)
+print('     sync: {0} {1} {2} {3} {4} {5}'.format(
+    y[0], y[1], y[2], y[3], y[-2], y[-1]))
+# print timing info
+print('----------')
+print('time(CPU)  : {0:.6f}'.format(t_cpu[0]))
+print('time(sync) : {0:.6f}'.format(t_sync[0]))
+print('time(async): {0:.6f}'.format(t_async[0]))
 
 # destroy BLAS handle
 destroy_handle(handle)
