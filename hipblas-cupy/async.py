c2b82f57c046460c8b066ad77010cebf64758716
Martti Louhivuori <martti.louhivuori@csc.fi>
2022-12-20 17:13:46 +0200

    Repeat BLAS call multiple times

--- hipblas-cupy/async.py
+++ hipblas-cupy/async.py
@@ -8,30 +8,41 @@ from _async import create_handle, destroy_handle
 
 n = 10000
 a = 3.4
+repeat = 5
 
-# initialise data and calculate reference values on CPU
+def axpy(a, x, y):
+    y += a * x
+
+# initialise data
 x = array([sin(i) * 2.3 for i in range(n)], float)
 y = array([cos(i) * 1.1 for i in range(n)], float)
-y_ref = a * x + y
+y_orig = y.copy()
 
 # allocate + copy initial values
 x_ = cupy.asarray(x)
 y_ = cupy.asarray(y)
 a_ = cupy.asarray(array([a], float))
 
+# calculate reference values on CPU
+for i in range(repeat):
+    axpy(a, x, y)
+
+# create BLAS handle
 handle = create_handle()
 
 # calculate axpy on GPU and keep data on GPUs
-daxpy_async(handle, n, a_.data.ptr, x_.data.ptr, y_.data.ptr)
+for i in range(repeat):
+    daxpy_async(handle, n, a_.data.ptr, x_.data.ptr, y_.data.ptr)
 
 # copy result back to host and print with reference
 print('  initial: {0} {1} {2} {3} {4} {5}'.format(
+    y_orig[0], y_orig[1], y_orig[2], y_orig[3], y_orig[-2], y_orig[-1]))
+print('reference: {0} {1} {2} {3} {4} {5}'.format(
     y[0], y[1], y[2], y[3], y[-2], y[-1]))
 y_.get(out=y)
-print('reference: {0} {1} {2} {3} {4} {5}'.format(
-    y_ref[0], y_ref[1], y_ref[2], y_ref[3], y_ref[-2], y_ref[-1]))
 print('   result: {0} {1} {2} {3} {4} {5}'.format(
     y[0], y[1], y[2], y[3], y[-2], y[-1]))
 
+# destroy BLAS handle
 destroy_handle(handle)
 del handle
